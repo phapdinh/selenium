87db1154c162ac77cd1c09c23cf20e03c2189cbb
Phap <centralparkloop@yahoo.com>
2019-01-17 22:11:24 -0500

    add gecko driver

--- src/test/java/com/selenium/BrowserTest.java
+++ src/test/java/com/selenium/BrowserTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserTest {
     public static void main(String[] args) {
+        String projectPath = System.getProperty("user.dir");
+        System.setProperty("webdriver.gecko.driver", projectPath + "/drivers/geckodriver/geckodriver.exe");
         String url = "https://ancient-springs-57162.herokuapp.com/";
         WebDriver driver = new FirefoxDriver();
         driver.get(url);
