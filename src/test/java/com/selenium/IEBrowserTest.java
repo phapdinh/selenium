72c92ad8cde9a34c74ad82c0c531431b93c73a2d
Phap <centralparkloop@yahoo.com>
2019-01-18 22:24:53 -0500

    added test for ie browser

--- src/test/java/com/selenium/IEBrowserTest.java
+++ src/test/java/com/selenium/IEBrowserTest.java
@@ -1,14 +1,14 @@
 package com.selenium;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
-public class ChromeBrowserTest {
+public class IEBrowserTest {
     public static void main(String[] args) {
         String projectPath = System.getProperty("user.dir");
-        System.setProperty("webdriver.chrome.driver", projectPath + "/drivers/chromedriver/chromedriver.exe");
+        System.setProperty("webdriver.ie.driver", projectPath + "/drivers/iedriver/IEDriverServer.exe");
         String url = "https://ancient-springs-57162.herokuapp.com/";
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new InternetExplorerDriver();
 
         driver.get(url);
     }
